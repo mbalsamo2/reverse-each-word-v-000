de96592c85c82f973d6ea665443c9312e36d3b0a
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 17:55:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string.split()
-  string.join(" ")
-  # the_statement = []
-  # array.collect { |word| the_statement << word }
+  the_statement = []
+  string.collect { |word| the_statement << word.reverse }
+  the_statement
 end
